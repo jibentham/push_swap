48b04bdd5fe0b34ada7086a68e49320d24e48e88
James Bentham <jbentham@f1r2s14.codam.nl>
2026-01-14 13:47:08 +0100

    Added indexers i and j to keep track of array positions for stack a and stack b respectively. Modified conditional for stack a to check whether there is more than one element in the stack, and added similar conditional for rule 'sb'.

--- push_swap_rules.c
+++ push_swap_rules.c
@@ -6,21 +6,36 @@
 /*   By: jbentham <jbentham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 13:25:01 by jbentham          #+#    #+#             */
-/*   Updated: 2026/01/14 13:32:02 by jbentham         ###   ########.fr       */
+/*   Updated: 2026/01/14 13:42:32 by jbentham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 do_rule(char *rule, int *stack_a, int *stack_b)
 {
 	int i;
+	int j;
 	int *temp;
 	
-
-	if (rule == "sa")
+	i = 0;
+	j = 0;
+	while (stack_a)
+		i++;
+	while (stack_b)
+		j++;
+	if (rule == "sa" && i >= 1)
 	{
 		temp[0] == stack_a[0];
 		stack_a[0] == stack_a[1];
 		stack_a[1] == temp[0];
 	}
+
+	if (rule == "sb" && j >= 1)
+	{
+		temp[0] == stack_b[0];
+		stack_b[0] == stack_b[1];
+		stack_b[1] == temp[0];
+	}
+
+	
 		
 }
