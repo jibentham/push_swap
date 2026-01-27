43eb5bc9cd38e8d95a8f71181f7ba917a9e1eaa9
James Bentham <jbentham@f0r3s16.codam.nl>
2026-01-27 04:55:04 +0100

    Added function 'to_top_cost()' in order to calculate the number of instructions  it takes for each node to be moved to the top of stack a. If positive, stack rotates normally, and if negative stack is reverse rotated.

--- push_swap_algo.c
+++ push_swap_algo.c
@@ -6,7 +6,7 @@
 /*   By: jbentham <jbentham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 18:19:01 by jbentham          #+#    #+#             */
-/*   Updated: 2026/01/22 20:19:19 by jbentham         ###   ########.fr       */
+/*   Updated: 2026/01/27 04:50:53 by jbentham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,31 @@ void sort_three(struct node **head_ref)
 		swap_node(head_ref);
 	if ((*head_ref)->element < (*head_ref)->next->element && (*head_ref)->element < (*head_ref)->next->next->element)
 		rotate_list(head_ref);
+}
+
+void to_top_cost(struct node **head_ref)
+{
+	int size;
+	int i;
+	struct node *tail;
+	struct node *parser;
+
+	size = 0;
+	i = 0;
+	tail = *head_ref;
+	parser = *head_ref;
+	while (tail != NULL)
+	{
+		tail = tail->next;
+		size++;
+	}
+	while (parser != NULL)
+	{
+		if (i <= size / 2)
+			parser->to_top_cost = i;
+		else
+			parser->to_top_cost = i - size;
+		parser = parser->next;
+		i++;
+	}
 }
