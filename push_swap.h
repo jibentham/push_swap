05a3331ff105beb6c06f1a355dee61134a15d854
jibentham <jibentham@pop-os.home>
2026-01-30 15:23:48 +0100

    Changed name of struct 'node' to 's_node' for norm compliance.

--- push_swap.h
+++ push_swap.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jibentham <jibentham@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/29 13:50:54 by jibentham         #+#    #+#             */
+/*   Updated: 2026/01/30 14:54:58 by jibentham        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
@@ -9,26 +21,26 @@
 # include <unistd.h>
 # include <string.h>
 
-struct node 
+struct s_node
 {
 	int element;
 	int to_top_cost;
 	int target;
-	struct node *next;
+	struct s_node *next;
 };
 
-struct node *create_node(char *element);
-void end_insert(struct node **head_ref, char *element);
-void head_insert(struct node **head_ref, char *element);
-void print_list(struct node *head);
+struct s_node *create_node(char *element);
+void end_insert(struct s_node **head_ref, char *element);
+//void head_insert(struct s_node **head_ref, char *element);
+void print_list(struct s_node *head);
 int	is_numeric(char *element);
-int no_duplicates(struct node **head);
-void push_node(struct node **src_ref, struct node **dest_ref);
-void swap_node(struct node **head);
-void rotate_list(struct node **head);
-void reverse_rotate_list(struct node **head);
-void min_max(struct node **head_a, struct node **head_b);
-void sort_three(struct node **head_ref);
-void to_top_cost(struct node **head_ref);
+int no_duplicates(struct s_node *head);
+void push_node(struct s_node **src_ref, struct s_node **dest_ref);
+void swap_node(struct s_node **head);
+void rotate_list(struct s_node **head);
+void reverse_rotate_list(struct s_node **head);
+void min_max(struct s_node **head_a, struct s_node **head_b);
+void sort_three(struct s_node **head_ref);
+void to_top_cost(struct s_node *head_ref);
 
 #endif
