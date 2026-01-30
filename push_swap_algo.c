48d962efb48e274e82e31d300ea6e7aa5fdb92a3
jibentham <jibentham@pop-os.home>
2026-01-30 15:21:07 +0100

    Added helper function to find the largest element in stack b, to assist with finding the target node for each node in stack a for optimizing sort. If the element of node a is bigger than the largest element in b, then a's target node is said largest element.

--- push_swap_algo.c
+++ push_swap_algo.c
@@ -3,42 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap_algo.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jbentham <jbentham@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jibentham <jibentham@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 18:19:01 by jbentham          #+#    #+#             */
-/*   Updated: 2026/01/27 04:50:53 by jbentham         ###   ########.fr       */
+/*   Updated: 2026/01/30 15:07:29 by jibentham        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void min_max(struct node **head_a, struct node **head_b)
-{
-	push_node(head_a, head_b);
-	push_node(head_a, head_b);
-	if ((*head_b)->element < (*head_b)->next->element)
-		swap_node(head_b);
-}
-
-void sort_three(struct node **head_ref)
-{
-	if ((*head_ref)->element < (*head_ref)->next->element && (*head_ref)->element > (*head_ref)->next->next->element)
-		swap_node(head_ref);
-	if ((*head_ref)->element < (*head_ref)->next->element && (*head_ref)->element < (*head_ref)->next->next->element)
-		rotate_list(head_ref);
-}
-
-void to_top_cost(struct node **head_ref)
+void to_top_cost(struct s_node *head_ref)
 {
 	int size;
 	int i;
-	struct node *tail;
-	struct node *parser;
+	struct s_node *tail;
+	struct s_node *parser;
 
 	size = 0;
 	i = 0;
-	tail = *head_ref;
-	parser = *head_ref;
+	tail = head_ref;
+	parser = head_ref;
 	while (tail != NULL)
 	{
 		tail = tail->next;
@@ -53,4 +37,25 @@ void to_top_cost(struct node **head_ref)
 		parser = parser->next;
 		i++;
 	}
+}
+
+struct s_node *find_largest_element(struct s_node *head_ref)
+{
+	struct s_node *temp;
+
+	if (head_ref == NULL)
+		return (NULL);
+	temp = head_ref;
+	while (head_ref != NULL)
+	{
+		if (head_ref->element > temp->element)
+			temp = head_ref;
+		head_ref = head_ref->next;
+	}
+	return (temp);
+}
+
+struct s_node *find_target(struct s_node *head_a, struct s_node *head_b)
+{
+	
 }
