6b3976233036e6b96d8d0a5bcd46e97ee8ee70ee
jibentham <jibentham@pop-os.home>
2026-02-04 10:28:29 +0100

    Modified file 'main.c' to implement full sort.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jibentham <jibentham@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 14:30:29 by jibentham         #+#    #+#             */
-/*   Updated: 2026/01/30 14:55:32 by jibentham        ###   ########.fr       */
+/*   Updated: 2026/02/03 16:02:43 by jibentham        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 int main (int argc, char **argv)
 {
 	struct s_node *head_a;
-	//struct s_node *head_b;
+	struct s_node *head_b;
+	struct s_node *cheapest;
 	int i;
 
 	head_a = NULL;
-	//head_b = NULL;
+	head_b = NULL;
+	cheapest = NULL;
 	i = 1;
 	while (i < argc)
 	{
@@ -37,6 +39,25 @@ int main (int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
+	if (head_a == NULL)
+		return (1);
+	while (stack_size(head_a) > 0)
+	{
+		to_top_cost(head_a);
+		add_target(head_a, head_b);
+		to_target_cost(head_a, head_b);
+		total_cost(head_a);
+
+		cheapest = find_cheapest(head_a);
+		execute_operations(&head_a, &head_b, cheapest);
+	}
+	while (head_b->element < head_b->next->element)
+		rotate_list(&head_b);
+	while (stack_size(head_b) > 0)
+		push_node(&head_b, &head_a);
+	while(head_a-> element < find_largest_element(head_a)->element)
+		rotate_list(&head_a);
+	rotate_list(&head_a);
 	print_list(head_a);
 	return (0);
 }
