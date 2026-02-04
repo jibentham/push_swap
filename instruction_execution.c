210adfc55d4cc590735bffe674b9aaffed43e1dc
jibentham <jibentham@pop-os.home>
2026-02-04 10:26:10 +0100

    Added file 'instruction_execution.c' for executing the sorting algorithm component of the codebase. Added function 'execute_operations()' for determining the order of execution based on conditional logic.

---
+++ instruction_execution.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jibentham <jibentham@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/02 13:58:27 by jibentham         #+#    #+#             */
+/*   Updated: 2026/02/02 16:21:43 by jibentham        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void execute_operations(struct s_node **head_a, struct s_node **head_b, struct s_node *cheapest)
+{
+    while (cheapest->to_top_cost > 0 && cheapest->to_target_cost > 0)
+    {
+        rotate_both(head_a, head_b);
+        cheapest->to_top_cost--;
+        cheapest->to_target_cost--;
+    }
+    while (cheapest->to_top_cost < 0 && cheapest->to_target_cost < 0)
+    {
+        reverse_rotate_both(head_a, head_b);
+        cheapest->to_top_cost++;
+        cheapest->to_target_cost++;
+    }
+    while (cheapest->to_top_cost > 0)
+    {
+        rotate_list(head_a);
+        cheapest->to_top_cost--;
+    }
+    while (cheapest->to_top_cost < 0)
+    {
+        reverse_rotate_list(head_a);
+        cheapest->to_top_cost++;
+    }
+    while (cheapest->to_target_cost > 0)
+    {
+        rotate_list(head_b);
+        cheapest->to_target_cost--;
+    }
+    while (cheapest->to_target_cost < 0)
+    {
+        reverse_rotate_list(head_b);
+        cheapest->to_target_cost++;
+    }
+    push_node(head_a, head_b);
+}
