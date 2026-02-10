6575cd7b965654a2764496e4fa0aa5b77160dd69
James Bentham <jbentham@f1r5s20.codam.nl>
2026-02-10 17:42:53 +0100

    Updated main function.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jbentham <jbentham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 14:30:29 by jibentham         #+#    #+#             */
-/*   Updated: 2026/02/04 12:26:06 by jbentham         ###   ########.fr       */
+/*   Updated: 2026/02/10 17:14:53 by jbentham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,19 +44,28 @@ int	main(int argc, char **argv)
 	while (stack_size(head_a) > 0)
 	{
 		to_top_cost(head_a);
+		to_top_cost(head_b);
 		add_target(head_a, head_b);
-		to_target_cost(head_a, head_b);
+		to_target_cost(head_a);
 		total_cost(head_a);
 		cheapest = find_cheapest(head_a);
 		execute_operations(&head_a, &head_b, cheapest);
 	}
-	while (head_b->element < head_b->next->element)
-		rotate_list(&head_b);
 	while (stack_size(head_b) > 0)
+	{
 		push_node(&head_b, &head_a);
-	while (head_a-> element < find_largest_element(head_a)->element)
+		printf("pa\n");
+	}
+	while (head_a-> element > find_smallest_element(head_a)->element)
+	{
 		rotate_list(&head_a);
-	rotate_list(&head_a);
-	print_list(head_a);
+		printf("ra\n");
+	}
+	// print_list(head_a);
+	while (head_a != NULL)
+	{
+		free(head_a);
+		head_a = head_a->next;
+	}
 	return (0);
 }
