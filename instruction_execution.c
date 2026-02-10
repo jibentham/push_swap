25445c315d3bead1aa90e54c187227d60df8456f
James Bentham <jbentham@f1r5s20.codam.nl>
2026-02-10 17:33:40 +0100

    Edited function 'execute_operations()' to improve readability.

--- instruction_execution.c
+++ instruction_execution.c
@@ -6,7 +6,7 @@
 /*   By: jbentham <jbentham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 13:58:27 by jibentham         #+#    #+#             */
-/*   Updated: 2026/02/04 12:36:59 by jbentham         ###   ########.fr       */
+/*   Updated: 2026/02/04 23:23:19 by jbentham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,49 @@
 
 void	execute_operations(struct s_node **head_a, struct s_node **head_b, struct s_node *cheapest)
 {
-	while (cheapest->to_top_cost > 0 && cheapest->to_target_cost > 0)
+	int a;
+	int b;
+	
+	a = cheapest->to_top_cost;
+	b = cheapest->to_target_cost;
+	while (a > 0 && b > 0)
 	{
 		rotate_both(head_a, head_b);
-		cheapest->to_top_cost--;
-		cheapest->to_target_cost--;
+		a--;
+		b--;
+		printf("rr\n");
 	}
-	while (cheapest->to_top_cost < 0 && cheapest->to_target_cost < 0)
+	while (a < 0 && b < 0)
 	{
 		reverse_rotate_both(head_a, head_b);
-		cheapest->to_top_cost++;
-		cheapest->to_target_cost++;
+		a++;
+		b++;
+		printf("rrr\n");
 	}
-	while (cheapest->to_top_cost > 0)
+	while (a > 0)
 	{
 		rotate_list(head_a);
-		cheapest->to_top_cost--;
+		a--;
+		printf("ra\n");
 	}
-	while (cheapest->to_top_cost < 0)
+	while (a < 0)
 	{
 		reverse_rotate_list(head_a);
-		cheapest->to_top_cost++;
+		a++;
+		printf("rra\n");
 	}
-	while (cheapest->to_target_cost > 0)
+	while (b > 0)
 	{
 		rotate_list(head_b);
-		cheapest->to_target_cost--;
+		b--;
+		printf("rb\n");
 	}
-	while (cheapest->to_target_cost < 0)
+	while (b < 0)
 	{
 		reverse_rotate_list(head_b);
-		cheapest->to_target_cost++;
+		b++;
+		printf("rrb\n");
 	}
 	push_node(head_a, head_b);
+	printf("pb\n");
 }
