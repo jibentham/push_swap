7eb03a40759aa3865248b402fc5bc7bb4b58403f
James Bentham <jbentham@f0r2s13.codam.nl>
2026-02-17 22:17:24 +0100

    Included header for libft printf.

--- costs_utils.c
+++ costs_utils.c
@@ -6,11 +6,12 @@
 /*   By: jbentham <jbentham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 16:32:40 by jibentham         #+#    #+#             */
-/*   Updated: 2026/02/13 15:16:13 by jbentham         ###   ########.fr       */
+/*   Updated: 2026/02/17 21:58:36 by jbentham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "libftprintf/ft_printf.h"
 
 t_node	*find_largest_element(t_node *head_ref)
 {
