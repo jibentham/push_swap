b01d4fd7ea80d73245d8e5c922c2a3933786ed9c
James Bentham <jbentham@f0r2s13.codam.nl>
2026-02-17 22:19:44 +0100

    Added integer overflow check in helper function 'check_overflow()'.

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,17 +6,19 @@
 /*   By: jbentham <jbentham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 17:22:32 by jbentham          #+#    #+#             */
-/*   Updated: 2026/02/10 17:22:36 by jbentham         ###   ########.fr       */
+/*   Updated: 2026/02/17 22:08:57 by jbentham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "libftprintf/ft_printf.h"
 
 int	ft_atoi(const char *nptr)
 {
 	int	i;
 	int	sign;
 	int	sum;
+	int	digit;
 
 	i = 0;
 	sign = 1;
@@ -31,8 +33,24 @@ int	ft_atoi(const char *nptr)
 	}
 	while ((nptr[i] >= '0') && (nptr[i] <= '9'))
 	{
-		sum = (sum * 10) + (nptr[i] - '0');
+		digit = nptr[i] - '0';
+		check_overflow(sum, digit, sign);
+		sum = (sum * 10) + digit;
 		i++;
 	}
 	return (sign * sum);
+}
+
+void	check_overflow(int sum, int digit, int sign)
+{
+	if (sign == 1 && sum > (INT_MAX - digit) / 10)
+	{
+		ft_printf("Error\n");
+		exit(EXIT_FAILURE);
+	}
+	if (sign == -1 && sum > ((long)INT_MAX + 1 - digit) / 10)
+	{
+		ft_printf("Error\n");
+		exit(EXIT_FAILURE);
+	}
 }
