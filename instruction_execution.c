c8a515d3a0a62109c8c30f861f3f95abc56a004f
James Bentham <jbentham@f0r2s13.codam.nl>
2026-02-17 22:20:47 +0100

    Pared execution function down by adding seperate helper funcitons for forward and reverse rotations.

--- instruction_execution.c
+++ instruction_execution.c
@@ -6,13 +6,36 @@
 /*   By: jbentham <jbentham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 13:58:27 by jibentham         #+#    #+#             */
-/*   Updated: 2026/02/13 15:20:47 by jbentham         ###   ########.fr       */
+/*   Updated: 2026/02/17 22:09:05 by jbentham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "libftprintf/ft_printf.h"
 
-void	execute_operations(t_node **head_a, t_node **head_b, t_node *cheapest)
+void	rotate_fwd(t_node **head, int *cost, char *fwd,
+		void (*rotate)(t_node **))
+{
+	while (*cost > 0)
+	{
+		rotate(head);
+		(*cost)--;
+		ft_printf("%s\n", fwd);
+	}
+}
+
+void	rotate_rev(t_node **head, int *cost, char *rev,
+		void (*rev_rotate)(t_node **))
+{
+	while (*cost < 0)
+	{
+		rev_rotate(head);
+		(*cost)++;
+		ft_printf("%s\n", rev);
+	}
+}
+
+void	execute_instructions(t_node **head_a, t_node **head_b, t_node *cheapest)
 {
 	int	a;
 	int	b;
@@ -24,39 +47,19 @@ void	execute_operations(t_node **head_a, t_node **head_b, t_node *cheapest)
 		rotate_both(head_a, head_b);
 		a--;
 		b--;
-		printf("rr\n");
+		ft_printf("rr\n");
 	}
 	while (a < 0 && b < 0)
 	{
 		reverse_rotate_both(head_a, head_b);
 		a++;
 		b++;
-		printf("rrr\n");
+		ft_printf("rrr\n");
 	}
-	while (a > 0)
-	{
-		rotate_list(head_a);
-		a--;
-		printf("ra\n");
-	}
-	while (a < 0)
-	{
-		reverse_rotate_list(head_a);
-		a++;
-		printf("rra\n");
-	}
-	while (b > 0)
-	{
-		rotate_list(head_b);
-		b--;
-		printf("rb\n");
-	}
-	while (b < 0)
-	{
-		reverse_rotate_list(head_b);
-		b++;
-		printf("rrb\n");
-	}
+	rotate_fwd(head_a, &a, "ra", rotate_list);
+	rotate_rev(head_a, &a, "rra", reverse_rotate_list);
+	rotate_fwd(head_b, &b, "rb", rotate_list);
+	rotate_rev(head_b, &b, "rrb", reverse_rotate_list);
 	push_node(head_a, head_b);
-	printf("pb\n");
+	ft_printf("pb\n");
 }
